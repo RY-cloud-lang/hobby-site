d6d3225d364743396c95927bdc696a47a0de9a0f
RY-cloud-lang <yonezawa1011ryuhei@yahoo.co.jp>
2025-10-28 14:17:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -111,6 +111,7 @@ function createTableContents(records) {
     const th = document.createElement("th");
     th.textContent = key; // 各ヘッダーセルにカラム名を設定
     th.dataset.type = DATA_TYPES[key]; // データ型情報を dataset に与える
+    th.dataset.spDisplay = DISPLAIES_FOR_SP[key]; // スマホの表示情報を dataset に与える
     th.addEventListener("click", function () {
       setSort(th, records);
     });
@@ -147,6 +148,7 @@ function createTableBodyRows(tbody, records, keyword) {
 
     for (let key in record) {
       const td = document.createElement("td");
+      td.dataset.spDisplay = DISPLAIES_FOR_SP[key]; // スマホの表示情報を dataset に与える
       const text = record[key];
 
       // keywordが指定されている場合、キーワードを強調表示する
